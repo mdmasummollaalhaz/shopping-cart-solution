37377f5114f5c7b8ba7c415ee053aef77386d66e
Masum Mollo <freelancermasummolla@gmail.com>
2022-02-15 11:11:48 +0600

    calculate products

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
-function updateCaseNumber(isIncreasing){
-    const caseInput = document.getElementById('case-number');
-    let caseNumber = caseInput.value;
+// Products function
+function updateProductNumber(product, price, isIncreasing){
+    const productInput = document.getElementById(product + '-number');
+    let productNumber = productInput.value;
     if(isIncreasing == true){
-        caseNumber = parseInt(caseNumber) + 1;
+        productNumber = parseInt(productNumber) + 1;
     }
-    else if(caseNumber > 0){
-        caseNumber = parseInt(caseNumber) - 1;
+    else if(productNumber > 1){
+        productNumber = parseInt(productNumber) - 1;
     }
-    caseInput.value = caseNumber;
+    productInput.value = productNumber;
 
     // Update case total
-    const caseTotal = document.getElementById('case-total');
-    caseTotal.innerText = caseNumber * 50;
+    const productTotal = document.getElementById(product + '-total');
+    productTotal.innerText = productNumber * price;
 }
 
 
+// Phone increase & decrease events
+document.getElementById('phone-plus').addEventListener('click', function(){
+    updateProductNumber('phone', 1200, true);
+})
+document.getElementById('phone-minus').addEventListener('click', function(){
+    updateProductNumber('phone', 1200, false)
+})
+
+
+
+// handle case increase & decrease events
 // Number Increage
 document.getElementById('case-plus').addEventListener('click', function(){
     // console.log('Checked');
@@ -23,7 +35,7 @@ document.getElementById('case-plus').addEventListener('click', function(){
     const caseNumber = caseInput.value;
     caseInput.value = parseInt(caseNumber) + 1;
     */
-    updateCaseNumber(true);
+    updateProductNumber('case', 50, true);
     
 })
 
@@ -33,5 +45,5 @@ document.getElementById('case-minus').addEventListener('click', function(){
     const caseInput = document.getElementById('case-number');
     const caseNumber = caseInput.value;
     */
-    updateCaseNumber(false);
+    updateProductNumber('case', 50, false);
 })
